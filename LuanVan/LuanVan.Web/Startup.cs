0c924ef6dba5982eb7a9adec1e94eefa66c3e28c
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-02-08 12:59:53 +0700

    Innit

---
+++ LuanVan/LuanVan.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LuanVan.Web.Startup))]
+namespace LuanVan.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
